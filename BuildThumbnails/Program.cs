a0648cc3f37860cb95f4b03f7775da14e0d03a13
Sridhar <sridhar_0314@live.com>
2025-02-12 13:31:46 -0500

    Fixes PHOT-4
    A new class BucketHandler was created.
    Method to check if photos bucket exists was created.
    Logger and dependency injection was added to the applciation.

--- BuildThumbnails/Program.cs
+++ BuildThumbnails/Program.cs
@@ -1,19 +1,49 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using BuildThumbnails.Services;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
 
-var configuration = new ConfigurationBuilder()
+IServiceCollection services = new ServiceCollection();
+//Configuration
+IConfiguration configuration = new ConfigurationBuilder()
     .SetBasePath(AppContext.BaseDirectory)
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
     .Build();
+services.AddSingleton(configuration);
+//Logger
+string logFileName = Path.Combine(Path.GetTempPath(), "BuildThumbnails-.txt");
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File(logFileName, rollingInterval: RollingInterval.Day, retainedFileCountLimit: 3)
+    .CreateLogger();
+services.AddLogging(c =>
+{
+    c.SetMinimumLevel(LogLevel.Information);
+    c.AddSerilog(Log.Logger);
+});
 
-// Read settings from the configuration
-IConfigurationSection appSettings = configuration.GetSection("AppSettings");
-string applicationName = appSettings["ApplicationName"] ?? "";
-string version = appSettings["Version"] ?? "";
-int maxItems = int.Parse(appSettings["MaxItems"] ?? "0");
+//Dependency Injection
+services.AddTransient<BucketHandler>();
 
-// Display the settings
-Console.WriteLine($"Application Name: {applicationName}");
-Console.WriteLine($"Version: {version}");
-Console.WriteLine($"Max Items: {maxItems}");
+//Build ServiceProvider
+ServiceProvider serviceProvider = services.BuildServiceProvider();
+
+BucketHandler? bucketHandler = serviceProvider.GetService<BucketHandler>();
+
+if (bucketHandler != null)
+{
+    bool bucketExists = await bucketHandler.CheckIfPhotosBucketExists();
+    if (!bucketExists)
+    {
+        Log.Logger.Error("Bucket not found");
+        Environment.Exit(1);
+    }
+}
+else
+{
+    Log.Logger.Error("BucketHandler not found");
+    Environment.Exit(1);
+}
---
+++ BuildThumbnails/Services/BucketHandler.cs
@@ -0,0 +1,31 @@
+﻿using Amazon;
+using Amazon.S3;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace BuildThumbnails.Services
+{
+    public class BucketHandler(IConfiguration configuration, ILogger<BucketHandler> logger)
+    {
+        private readonly IConfiguration configuration = configuration;
+        private readonly ILogger<BucketHandler> logger = logger;
+
+        public async Task<bool> CheckIfPhotosBucketExists()
+        {
+            //Get the bucket name from the configuration
+            var bucketName = configuration["AWSS3:PhotoBucketName"];
+            //Check if the bucket exists
+            using var s3Client = new AmazonS3Client(RegionEndpoint.GetBySystemName(configuration["AWSS3:PhotoBucketRegion"]));
+            try
+            {
+                var response = await s3Client.ListBucketsAsync();
+                return response.Buckets.Any(b => b.BucketName.Equals(bucketName, StringComparison.OrdinalIgnoreCase));
+            }
+            catch (AmazonS3Exception ex)
+            {
+                logger.LogError(ex, "Error checking if bucket exists");
+            }
+            return false;
+        }
+    }
+}
